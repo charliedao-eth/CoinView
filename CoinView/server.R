50f07206a6429dfba8de932c6a4b0695ed73467b
Carlos R. Mercado <fairlabs@protonmail.com>
2022-01-21 14:20:58 -0500

    App_Setup

---
+++ CoinView/server.R
@@ -0,0 +1,10 @@
+
+library(shiny)
+library(plotly)
+library(quantmod)
+
+# Define server logic 
+shinyServer(function(input, output) {
+
+
+})
---
+++ CoinView/ui.R
@@ -0,0 +1,21 @@
+
+library(shiny)
+library(plotly)
+library(quantmod)
+
+# Define UI for application
+shinyUI(fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  
+  div(class = ''
+  ),
+  
+  div(),
+  div(),
+  div(),
+  div(),
+  div()
+  
+))
