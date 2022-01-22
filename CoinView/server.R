98652973765c3ff776adb347ee015b146e85d67c
Carlos R. Mercado <fairlabs@protonmail.com>
2022-01-22 10:17:59 -0500

    Initiating Application - 1
    Buttons, Current Cycle, Cycle Length Singular, Candle-Fib, and RSI completed and aligned.

--- CoinView/server.R
+++ CoinView/server.R
@@ -2,9 +2,75 @@
 library(shiny)
 library(plotly)
 library(quantmod)
+library(DT)
+source("global.R")
 
 # Define server logic 
-shinyServer(function(input, output) {
-
+shinyServer(function(input, output, session) {
+  # Generate Data Transforms 
+  
+  ftx <- reactive({ 
+    df <- template
+    df$startTime <- as.POSIXct(df$startTime)
+    df <- add_fib(df)
+    df <- add_fib_status(df)
+    df
+  })
+  
+  ftx_sequence <- reactive({ 
+    get_fib_sequences(ftx())
+    })
+  
+  observe({ 
+    ff <<- ftx_sequence()
+    })
+  
+  # Cycle Trend ----
+  output$cycle_trend <- DT::renderDataTable({
+    cycle_table(ftx_sequence())
+  })
+  
+  # Single Cycle Length ----
+  output$cycle_single <- renderPlotly({
+    get_single_history(ftx_sequence(),
+                       ftx_sequence()$cl$current_status,
+                       ftx_sequence()$cl$current_length)
+  })
+  
+  # Fib Candle ----
+  output$candle <- renderPlotly({
+    get_candle(ftx(), resolution = input$resolution, add_fib = TRUE)
+  })
+  
+  # RSI ----
+  output$rsi <- renderPlotly({ 
+    get_RSI(ftx())
+    })
+  
+  # MACD ----
+  
+  # Historical ---- 
+  
+  
+  
+  
+  # Error Prevention ----
+   # Only allow USD as Quote Currency on certain Base Currencies
+   # To conform to available FTX Markets 
+  
+  observeEvent(input$base_currency, {
+    
+    if(input$base_currency %in% c("AAVE","BAT","DAI","GRT","MKR","SHIB")){ 
+      updateSelectInput(session = session,
+                        inputId = "quote_currency",
+                        label = "Quote Currency",
+                        choices = "USD")
+    } else { 
+      updateSelectInput(session = session,
+                        inputId = "quote_currency",
+                        label = "Quote Currency",
+                        choices = readLines("available_options/QUOTECURRENCY.txt"))
+      }
+  })
 
 })
--- CoinView/ui.R
+++ CoinView/ui.R
@@ -2,20 +2,89 @@
 library(shiny)
 library(plotly)
 library(quantmod)
+library(DT)
+source("global.R")
 
 # Define UI for application
-shinyUI(fluidPage(
+shinyUI(ui = fluidPage(
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+    ),
+  
+  div(class = 'main-app',
+      div(class = "button-bar",
+          fluidRow(
+            column(2, 
+                   selectInput(inputId = "base_currency",
+                               label = "Base Currency",
+                               choices = readLines("available_options/BASECURRENCY.txt"),
+                               selected = "ETH",
+                               multiple = FALSE,
+                               selectize = TRUE)),
+            column(2, 
+                   selectInput(inputId = "quote_currency",
+                               label = "Quote Currency",
+                               choices = readLines("available_options/QUOTECURRENCY.txt"),
+                               selected = "ETH",
+                               multiple = FALSE,
+                               selectize = TRUE)),
+            column(2, selectInput(inputId = "resolution",
+                                 label = "Resolution",
+                                 choices = list(
+                                   "15 Seconds" = "segment",
+                                   "1 Minute" = "minute",
+                                   "5 Minutes" = "five_minute",
+                                   "15 Minutes" = "fifteen_minute",
+                                   "1 Hour" = "hour",
+                                   "4 Hours" = "four_hour",
+                                   "1 Day" = "day",
+                                   "1 Week" = "week"),
+                                 selected = "ETH",
+                                 multiple = FALSE,
+                                 selectize = TRUE)),
+            column(2, dateInput(inputId = "start_date",
+                                label = "Start Date",
+                                value = "2021-01-01",
+                                min = "2019-09-13",
+                                max = Sys.Date())),
+            column(2,dateInput(inputId = "end_date",
+                             label = "End Date",
+                             value = Sys.Date(),
+                             min = "2019-09-14",
+                             max = Sys.Date())),
+            column(2, 
+                   br(),
+                   div(class = "center-button",
+                   actionButton(inputId = "begin",
+                                label = "Launch", width = "80%"))
+            )
+          )),
+      div(class = "current-market",
+          fluidRow(
+            column(6, 
+                   div(class = "current-market-table",
+                       DT::dataTableOutput("cycle_trend")
+                   )),
+            column(6, div(class = "current-market-boxplot",
+                       plotlyOutput("cycle_single", height = 150)
+                   ))
+          )),
+      div(class = "graphs-section",
+          div(class = "candle chart",
+              plotlyOutput("candle", width = "100%", height = 300)
+              ),
+          div(class = "rsi chart",
+              plotlyOutput("rsi", width = "100%", height = 300)
+              ),
+          div(class = "macd chart",
+              hr()
+              ),
+          div(class = "historic chart",
+              hr()
+              )
+          )
   ),
-  
-  div(class = ''
-  ),
-  
-  div(),
-  div(),
-  div(),
-  div(),
-  div()
-  
+  div(class = "footer", 
+      hr()
+      )
 ))
